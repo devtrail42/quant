7ff31370cc5da7df33bda5c942111d27bac7aa47
yongbeom.cho <scr3115@gmail.com>
2025-12-09 19:33:15 +0900

    ohlcv_data to sqlite3 db (data error solved)

--- sbin/01_get_daily_ohlcv_data.sh
+++ sbin/01_get_daily_ohlcv_data.sh
@@ -8,14 +8,15 @@ if [ -z "$root_dir" ]; then
 fi
 
 if [ -z "$date_arg" ]; then
-    date_arg=$(date -v-1d +%Y%m%d)
+    # date_arg=$(date -v-1d +%Y%m%d)
+    date_arg=all
 fi
 
-echo "$root_dir $date_arg"
 ##--interval : [day, minute1, minute3, minute5, minute10, minute15, minute30, minute60, minute240, week, month]
 market=coin
-output_dir="${root_dir}/var/data/ohlcv_${interval}"
-
+output_dir="${root_dir}/var/data"
+# interval=minute1
+# python ${root_dir}/sbin/data_pipeline/01_get_daily_ohlcv_data.py --root_dir ${root_dir} --date ${date_arg} --market ${market} --interval ${interval} --output_dir ${output_dir}
 for interval in day minute1 minute3 minute5 minute10 minute15 minute30 minute60 minute240 week month; do
-    python ${root_dir}/sbin/data_pipeline/01_get_daily_ohlcv_data.py --date ${date} --market ${market} --interval ${interval} --output_dir ${root_dir}var/data/ohlcv_${x} 
+    python ${root_dir}/sbin/data_pipeline/01_get_daily_ohlcv_data.py --root_dir ${root_dir} --date ${date_arg} --market ${market} --interval ${interval} --output_dir ${output_dir}
 done
